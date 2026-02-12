b45403c29e8696f2406d1e563173daa9df930c15
Netra Pal Gangwar <arten.discovery@gmail.com>
2026-02-12 09:13:31 +0530

    remove

--- conftest.py
+++ conftest.py
@@ -41,9 +41,4 @@ def role_selected(language_selected):
     role.click_proceed()
     return language_selected
 
-@pytest.fixture
-def role_selected(language_selected):
-    role = RolePage(language_selected)
-    role.select_role("Teacher")   # 👈 UPDATED
-    role.click_proceed()
-    return language_selected
+
